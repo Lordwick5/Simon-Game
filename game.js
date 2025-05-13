35eee4eba63058dddb80e748e3466653a6a83eae
Lordwick5 <Prashantchy96@gmail.com>
2025-05-13 06:53:28 +0545

    updated code

--- game.js
+++ game.js
@@ -4,4 +4,6 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 }
