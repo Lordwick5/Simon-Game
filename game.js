af1d2333e48482b56c9340aa49b35ed30a78d363
Lordwick5 <Prashantchy96@gmail.com>
2025-05-13 06:43:26 +0545

    updated code

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
+var gamePattern;
 var buttonColours = ["red", "blue", "green", "yellow"];
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
-
-    
+    var randomChosenColour = randomNumber(buttonColours);
 }
